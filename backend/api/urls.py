e810ae0e1ab7386390ebef267857be6a6426defa
User <st08kz@yandex.kz>
2024-10-12 15:10:07 +0500

    urls

---
+++ backend/api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
+                    TagViewSet, short_url)
+
+app_name = 'api'
+
+router = routers.DefaultRouter()
+router.register(r'users', CustomUserViewSet, basename='user')
+router.register(r'tags', TagViewSet, basename='tag')
+router.register(r'ingredients', IngredientViewSet, basename='ingredient')
+router.register(r'recipes', RecipeViewSet, basename='recipe')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('s/<str:url_hash>/', short_url, name='short_url')
+]
