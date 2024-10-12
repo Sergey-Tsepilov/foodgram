2c3f64ad992566ad3bbd905136b56ed228ab4290
User <st08kz@yandex.kz>
2024-10-12 15:16:22 +0500

    admin

---
+++ backend/urlshort/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import ShortLink
+
+
+@admin.register(ShortLink)
+class ShortLinkAdmin(admin.ModelAdmin):
+    """Админка для коротких ссылок."""
+
+    list_display = ('id', 'original_url', 'url_hash')
+    list_display_links = ('original_url',)
